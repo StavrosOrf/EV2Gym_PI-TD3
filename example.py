3c3daf48f1fdeda1dd27fabd23d09f06102583d3
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2025-02-10 12:21:11 +0100

    clearing up environment

--- example.py
+++ example.py
@@ -5,26 +5,20 @@ import random
 env_config = {
     "voltage_limits": [0.95, 1.05],
     "algorithm": "Laurent",
-    "battery_list": [],
-    "year": 2020,
-    "month": 1,
-    "day": 1,
+    "timescale": 15,
+    "episode_length": 5,
     "train": True,
-    "state_pattern": "default",
     "network_info": {'vm_pu': 1.0,
                      's_base': 1000,
                      'bus_info_file': './rl_adn/data_sources/network_data/node_34/Nodes_34.csv',
-                     'branch_info_file': './rl_adn/data_sources/network_data/node_34/Lines_34.csv'},    
+                     'branch_info_file': './rl_adn/data_sources/network_data/node_34/Lines_34.csv'},
     "time_series_data_path": "./rl_adn/data_sources/time_series_data/34_node_time_series.csv"
 }
 
-env=PowerNetEnv(env_config)
+env = PowerNetEnv(env_config)
 
 env_args = {
     'env_name': 'PowerNetEnv',
-    'state_dim': env.state_space.shape[0],
-    'action_dim': env.action_space.shape[0],
-    'if_discrete': False
 }
 
 print(env_args)
@@ -33,33 +27,43 @@ succesful_runs = 0
 failed_runs = 0
 
 for i in range(10000):
-    state = env.reset()
+    hour = 0
+    month = 7
+    day = 17
+    year = 2020
+    hour = 0
+    minute = 0
+    
+
+    date = (2020, month, day, hour, minute)
+    state = env.reset(date)
     done = False
     counter = 0
-    while not done:
+    for i in range(env_config['episode_length']):
+
         # print(f'=== Step {counter} ===')
         counter += 1
-        #action is 34 values between -100 and 100, it is the power injection in each node
-        action = [0]*33       
+        # action is 34 values between -100 and 100, it is the power injection in each node
+        action = [0]*33
         # action[32] = 150
-        action =random.sample(range(-1000, 1000), 33)
-        next_state, saved_energy, done = env.step(action)
+        # action =random.sample(range(-1000, 1000), 33)
+        next_state, saved_energy = env.step(action)
         # print("State: ", state)
         # print("Action: ", action)
         # print("Saved Energy: ", saved_energy)
         # print("Next State: ", next_state)
-        
-        if done:
-            
+
+        if i == env_config['episode_length'] - 2:
+            exit()
             succesful_runs += 1
             break
-        
-        if any(next_state[1] < 0.95) or any(next_state[1] > 1.05):
-            # print("Voltage limits exceeded")
-            done = True
-            failed_runs += 1
-          
+
+        # if any(next_state[1] < 0.95) or any(next_state[1] > 1.05):
+        #     print("Voltage limits exceeded step: ", counter)
+        #     done = True
+        #     failed_runs += 1
+
         state = next_state
-    
+
     if i % 100 == 0:
         print(f' Succesful runs: {succesful_runs} Failed runs: {failed_runs}')
--- rl_adn/data_manager/data_manager.py
+++ rl_adn/data_manager/data_manager.py
@@ -5,8 +5,6 @@ data loading, cleaning, and basic manipulations.
 """
 
 
-
-
 from typing import List, Tuple, Union
 import pandas as pd
 import numpy as np
@@ -56,7 +54,8 @@ class GeneralPowerDataManager:
         # Print data scale and initialize time interval
         min_date = data.index.min()
         max_date = data.index.max()
-        print(f"Data scale: from {min_date.strftime('%Y-%m-%d')} to {max_date.strftime('%Y-%m-%d')}")
+        print(
+            f"Data scale: from {min_date.strftime('%Y-%m-%d')} to {max_date.strftime('%Y-%m-%d')}")
 
         self.time_interval = int((data.index[1] - data.index[0]).seconds / 60)
         print(f"Data time interval: {self.time_interval} minutes")
@@ -65,11 +64,16 @@ class GeneralPowerDataManager:
         self.df = data
         self.data_array = data.values
 
-        self.active_power_cols = [col for col in self.df.columns if re.fullmatch(r'active_power(_\w+)?', col)]
-        self.reactive_power_cols = [col for col in self.df.columns if re.fullmatch(r'reactive_power(_\w+)?', col)]
-        self.renewable_active_power_cols = [col for col in self.df.columns if re.fullmatch(r'renewable_active_power(_\w+)?', col)]
-        self.renewable_reactive_power_cols = [col for col in self.df.columns if re.fullmatch(r'renewable_reactive_power(_\w+)?', col)]
-        self.price_col = [col for col in self.df.columns if re.fullmatch(r'price(_\w+)?', col)]
+        self.active_power_cols = [
+            col for col in self.df.columns if re.fullmatch(r'active_power(_\w+)?', col)]
+        self.reactive_power_cols = [
+            col for col in self.df.columns if re.fullmatch(r'reactive_power(_\w+)?', col)]
+        self.renewable_active_power_cols = [col for col in self.df.columns if re.fullmatch(
+            r'renewable_active_power(_\w+)?', col)]
+        self.renewable_reactive_power_cols = [col for col in self.df.columns if re.fullmatch(
+            r'renewable_reactive_power(_\w+)?', col)]
+        self.price_col = [
+            col for col in self.df.columns if re.fullmatch(r'price(_\w+)?', col)]
         # Display dataset information
         print(f"Dataset loaded from {datapath}")
         print(f"Dataset dimensions: {self.df.shape}")
@@ -82,13 +86,17 @@ class GeneralPowerDataManager:
             f"Renewable active power columns: {self.renewable_active_power_cols} (Indices: {[self.df.columns.get_loc(col) for col in self.renewable_active_power_cols]})")
         print(
             f"Renewable reactive power columns: {self.renewable_reactive_power_cols} (Indices: {[self.df.columns.get_loc(col) for col in self.renewable_reactive_power_cols]})")
-        print(f"Price columns: {self.price_col} (Indices: {[self.df.columns.get_loc(col) for col in self.price_col]})")
+        print(
+            f"Price columns: {self.price_col} (Indices: {[self.df.columns.get_loc(col) for col in self.price_col]})")
+
         # Calculate max and min for each type of power
         self.active_power_max = self.df[self.active_power_cols].max().max()
         self.active_power_min = self.df[self.active_power_cols].min().min()
 
-        self.reactive_power_max = self.df[self.reactive_power_cols].max().max() if self.reactive_power_cols else None
-        self.reactive_power_min = self.df[self.reactive_power_cols].min().min() if self.reactive_power_cols else None
+        self.reactive_power_max = self.df[self.reactive_power_cols].max(
+        ).max() if self.reactive_power_cols else None
+        self.reactive_power_min = self.df[self.reactive_power_cols].min(
+        ).min() if self.reactive_power_cols else None
 
         self.renewable_active_power_max = self.df[
             self.renewable_active_power_cols].max().max() if self.renewable_active_power_cols else None
@@ -99,15 +107,16 @@ class GeneralPowerDataManager:
             self.renewable_reactive_power_cols].max().max() if self.renewable_reactive_power_cols else None
         self.renewable_reactive_power_min = self.df[
             self.renewable_reactive_power_cols].min().min() if self.renewable_reactive_power_cols else None
-        self.price_min = self.df[self.price_col].min().values[0] if self.price_col else None
-        self.price_max = self.df[self.price_col].max().values[0] if self.price_col else None
+        self.price_min = self.df[self.price_col].min(
+        ).values[0] if self.price_col else None
+        self.price_max = self.df[self.price_col].max(
+        ).values[0] if self.price_col else None
         # split the train and test dates
         self.train_dates = []
         self.test_dates = []
         self.split_data_set()
         self._replace_nan()
         self._check_for_nan()
-
 
     def _replace_nan(self) -> None:
         """
@@ -122,22 +131,18 @@ class GeneralPowerDataManager:
         Check if any of the arrays contain NaN values and raise an error if they do
         """
         if self.df.isnull().sum().sum() > 0:
-            raise ValueError("Data still contains NaN values after preprocessing")
+            raise ValueError(
+                "Data still contains NaN values after preprocessing")
 
-    def select_timeslot_data(self, year: int, month: int, day: int, timeslot: int) -> np.ndarray:
-        """
-               Select data for a specific timeslot on a specific day.
+    def select_timeslot_data(self,
+                             year: int,
+                             month: int,
+                             day: int,
+                             hour: int,
+                             minute: int,
+                             timeslot: int) -> np.ndarray:
 
-               Parameters:
-                   year (int): The year of the date.
-                   month (int): The month of the date.
-                   day (int): The day of the date.
-                   timeslot (int): The timeslot index.
-
-               Returns:
-                   np.ndarray: The data for the specified timeslot.
-               """
-        dt = pd.Timestamp(year=year, month=month, day=day, hour=0, minute=0, second=0, tz='UTC') + pd.Timedelta(
+        dt = pd.Timestamp(year=year, month=month, day=day, hour=hour, minute=minute, second=0, tz='UTC') + pd.Timedelta(
             minutes=self.time_interval * timeslot)
         row = self.df.loc[dt]
         return row.values
@@ -154,7 +159,8 @@ class GeneralPowerDataManager:
         Returns:
             np.ndarray: The data for the specified day.
         """
-        start_dt = pd.Timestamp(year=year, month=month, day=day, hour=0, minute=0, second=0, tz='UTC')
+        start_dt = pd.Timestamp(year=year, month=month,
+                                day=day, hour=0, minute=0, second=0, tz='UTC')
         end_dt = start_dt + pd.Timedelta(days=1)
         day_data = self.df.loc[start_dt:end_dt - pd.Timedelta(minutes=1), :]
         return day_data.values
@@ -167,7 +173,8 @@ class GeneralPowerDataManager:
                    List[Tuple[int, int, int]]: A list of available dates as (year, month, day).
                """
         dates = self.df.index.strftime('%Y-%m-%d').unique()
-        year_month_day = [(int(date[:4]), int(date[5:7]), int(date[8:10])) for date in dates]
+        year_month_day = [(int(date[:4]), int(date[5:7]),
+                           int(date[8:10])) for date in dates]
         return year_month_day
 
     def random_date(self) -> Tuple[int, int, int]:
@@ -180,6 +187,7 @@ class GeneralPowerDataManager:
         dates = self.list_dates()
         year, month, day = random.choice(dates)
         return year, month, day
+
     def split_data_set(self):
         """
         Split the data into training and testing sets based on the date.
@@ -201,7 +209,8 @@ class GeneralPowerDataManager:
             if month != current_month or year != current_year:
                 # Sort monthly dates and split into train and test
                 monthly_dates.sort()
-                train_len = int(len(monthly_dates) * (3 / 4))  # First three weeks for training
+                # First three weeks for training
+                train_len = int(len(monthly_dates) * (3 / 4))
                 train_dates += monthly_dates[:train_len]
                 test_dates += monthly_dates[train_len:]
 
@@ -221,5 +230,3 @@ class GeneralPowerDataManager:
 
         self.train_dates = train_dates
         self.test_dates = test_dates
-
-
